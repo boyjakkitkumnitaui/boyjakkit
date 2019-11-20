8afd7a82f16ca186bedab7d5d67e3c03365312a3
boyjakkitkumnitaui <57611680+boyjakkitkumnitaui@users.noreply.github.com>
2019-11-20 22:30:01 +0700

    Delete chksess_b.php

--- chksess_b.php
+++
@@ -1,8 +0,0 @@
-<? session_start();
-header ('Content-Type: text/html; charset=windows-874');
-if (($sess_id <> session_id()) or ($SESS_BRANCH == '') or ($SESS_STOCK == '')) {
-	$massage = "Login Timeout Please Login Again";
-	echo "<meta http-equiv=refresh content=0;MLogin.php?massage=$massage></a></div>";
-	exit();
-
-}
